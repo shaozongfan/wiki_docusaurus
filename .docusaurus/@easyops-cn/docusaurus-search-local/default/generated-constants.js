8455d8d2c069491224da12bcb3eb2a7708231b26
shaozongfan <605719056@qq.com>
2025-12-27 15:23:47 +0800

    Deploy website - based on 5916a557048ffb28342e806f0c100de49faeb2da

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,9 +0,0 @@
-import lunr from "/workspace/projects/knowledge-wiki/node_modules/lunr/lunr.js";
-require("/workspace/projects/knowledge-wiki/node_modules/lunr-languages/lunr.stemmer.support.js")(lunr);
-require("/workspace/projects/knowledge-wiki/node_modules/@easyops-cn/docusaurus-search-local/dist/client/shared/lunrLanguageZh.js").lunrLanguageZh(lunr);
-require("/workspace/projects/knowledge-wiki/node_modules/lunr-languages/lunr.multi.js")(lunr);
-export const removeDefaultStopWordFilter = [];
-export const language = ["en","zh"];
-export const searchIndexUrl = "search-index{dir}.json?_=b620fdcc";
-export const searchResultLimits = 8;
-export const fuzzyMatchingDistance = 1;
--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/workspace/projects/knowledge-wiki/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/workspace/projects/knowledge-wiki/node_modules/infima/dist/css/default/default.css"),
-  require("/workspace/projects/knowledge-wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspace/projects/knowledge-wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspace/projects/knowledge-wiki/src/css/custom.css"),
-];
